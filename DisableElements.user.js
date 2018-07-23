8734219cb738157e737823c681dd55eca4aa88da
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-07-23 22:25:26 +0900

    非表示タイプをhiddenに変更

--- DisableElements.user.js
+++ DisableElements.user.js
@@ -9,39 +9,38 @@
 // };
 
 function disableElements(){
-    disableElementWhenOverThisZIndex(10);
-    disableThisTagNames(["iFrame"]);
-    disableElementsWhatIncludesThisHref(["traffic-url"]);
+    var mode = "hidden";
+    disableElementWhenOverThisZIndex(10, mode);
+    disableThisTagNames(["iFrame"], mode);
+    disableElementsWhatIncludesThisHref(["traffic-url"], mode);
 }
 
-function disableElementWhenOverThisZIndex(zidx){
+function disableElementWhenOverThisZIndex(zidx, mode){
     var tags = document.getElementsByTagName("*");
     for(i=0;i < tags.length;i++){
         var elem = tags[i];
         zIndexOfElem = document.defaultView.getComputedStyle(elem,null).zIndex; //z-Indexの取得
 
         if(zIndexOfElem > zidx){ //指定z-indexより大きかったら
-            elem.style.display = "none"; //display: none; を設定
-            console.log(`following element is disabled by "disableElementWhenOverThisZIndex". tagName:"${elem.tagName}", id:"${elem.id}", className:"${elem.className}"`);
+            disableThisElement(elem, mode, "disableElementWhenOverThisZIndex");
         }
     }
 }
 
-function disableThisTagNames(tagNames){
+function disableThisTagNames(tagNames, mode){
     var tags = document.getElementsByTagName("*");
     for(i=0;i < tags.length;i++){
         var elem = tags[i];
         for(var j = 0 ; j < tagNames.length ; j++){
             if(tagNames[j].toUpperCase() === elem.tagName.toUpperCase()){ //大文字・小文字の違いを無視して一致した場合
-                elem.style.display = "none"; //display: none; を設定
-                console.log(`following element is disabled by "disableThisTagNames. tagName:"${elem.tagName}", id:"${elem.id}", className:"${elem.className}"`);
+                disableThisElement(elem, mode, "disableThisTagNames");
                 break;
             }
         }
     }
 }
 
-function disableElementsWhatIncludesThisHref(refs){
+function disableElementsWhatIncludesThisHref(refs, mode){
     var tags = document.getElementsByTagName("*");
     for(i=0;i < tags.length;i++){
         var elem = tags[i];
@@ -56,8 +55,23 @@ function disableElementsWhatIncludesThisHref(refs){
             }
         }
         if(fnd){
-            elem.style.display = "none";
-            console.log(`following element is disabled by "disableElementsWhatIncludesThisHref. tagName:"${elem.tagName}", id:"${elem.id}", className:"${elem.className}, href:"${elem.getAttribute('href')}"`);
+            disableThisElement(elem, mode, "disableElementsWhatIncludesThisHref");
         }
     }
+}
+
+function disableThisElement(elem, mode, whoCalled){    
+    switch(mode){
+        case "node":
+            elem.style.display = "none";
+            break;
+
+        case "hidden":
+            elem.style.visibility = "hidden";
+            break;
+
+        default:
+            break;
+    }
+    console.log(`following element is disabled by "${whoCalled}". tagName:"${elem.tagName}", id:"${elem.id}", className:"${elem.className}, href:"${elem.getAttribute('href')}"`);
 }
