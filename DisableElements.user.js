fe9d76122a01d84c3f03858bfaa9cb85de969b24
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-11-26 21:09:28 +0900

    ver2

--- DisableElements.user.js
+++ DisableElements.user.js
@@ -1,123 +1,202 @@
-//document.addEventListener("DOMContentLoaded", function() {
-    // console.log("DOMContentLoaded");
-    // disableElements();
-//});
+(function () {
 
-window.onload = function(){
-    console.log("window.onload");
-    disableElements();
-};
-
-function disableElements(){
-    // var mode = "hidden";
-    // disableElementWhenOverThisZIndex(10, mode);
-    // disableThisTagNames(["iFrame"], mode);
-    // disableElementsWhatIncludesThisHref(["traffic-url"], mode);
-    disableElementsWhatIncludesThisIDs(["eobm_"],"none");
-    disableThisClasses(["exp-outline"], "none");
-
-}
-
-function disableElementWhenOverThisZIndex(zidx, mode){
-    var tags = document.getElementsByTagName("*");
-    for(i=0;i < tags.length;i++){
-        var elem = tags[i];
-        zIndexOfElem = document.defaultView.getComputedStyle(elem,null).zIndex; //z-Indexの取得
-
-        if(zIndexOfElem > zidx){ //指定z-indexより大きかったら
-            disableThisElement(elem, mode, "disableElementWhenOverThisZIndex");
-        }
-    }
-}
-
-function disableThisIDs(idNames, mode){
-    for(var i = 0 ; i < idNames.length ; i++){
-        var elem = document.getElementById(idNames[i]);
-        if(elem != null){ //大文字・小文字の違いを無視して一致した場合
-            disableThisElement(elem, mode, "disableThisIDs");
-        }
-    }
-}
-
-//todo
-function disableElementsWhatIncludesThisIDs(idNames, mode){
-    var tags = document.getElementsByTagName("*");
-    for(i=0;i < tags.length;i++){
-        var elem = tags[i];
-        var idName = elem.id;
-        for(var j = 0 ; j < idNames.length ; j ++){
-            var fnd = idName.indexOf(idNames[j]) != -1; //文字列が存在するかどうか
-            if(fnd){
-                disableThisElement(elem, mode, "disableElementsWhatIncludesThisIDs");
-                break;
+    var objArr1 = [
+        {
+            specifier: null,
+            mode:"none",
+            judger: function(elem){ //<div data-base-uri="???">の形式の場合にtrueを返す
+                if(elem.tagName.toUpperCase() != 'DIV'){
+                    return false;
+                }else{
+                    var baseUri = elem.getAttribute("data-base-uri");
+                    if(baseUri == '' || baseUri === null){
+                        return false;
+                    }else{
+                        return true;
+                    }
+                }
             }
         }
-        
-    }
-}
+    ];
 
-function disableThisClasses(classNames, mode){
-    for(var i = 0 ; i < classNames.length ; i++){
-        var elements = document.getElementsByClassName(classNames[i]);
-        console.log(elements);
-        if(elements != null){ //大文字・小文字の違いを無視して一致した場合
-            for(var j = 0; elements.length ; j++){
-                disableThisElement(elements[j], mode, "disableThisClasses");
-            }
+    var timingAndsettings = {
+
+        // Dom Content Loaded のタイミングで発火する
+        ready: objArr1,
+
+        // window.onloadのタイミングで発火する
+        // onload: objArr1,
+    };
+
+    //判定関数リスト
+    var judgers = new function(){
+    
+        // zIndexが指定値を超えているかどうか
+        this.over_zIndex = function(elem, val){
+            return(document.defaultView.getComputedStyle(elem,null).zIndex  > val);
         }
-    }
-}
-
-function disableThisTagNames(tagNames, mode){
-    var tags = document.getElementsByTagName("*");
-    for(i=0;i < tags.length;i++){
-        var elem = tags[i];
-        for(var j = 0 ; j < tagNames.length ; j++){
-            if(tagNames[j].toUpperCase() === elem.tagName.toUpperCase()){ //大文字・小文字の違いを無視して一致した場合
-                disableThisElement(elem, mode, "disableThisTagNames");
-                break;
+    
+        // 指定文字列をクラス名を持っているかどうか
+        this.eq_class = function(elem, str){
+            return (elem.classList.contains(str));
+        }
+    
+        // 指定ID名かどうか
+        this.eq_id = function(elem, str){
+            return (elem.id == str);
+        }
+    
+        // ID名に指定文字列が含まれるかどうか
+        this.inc_id = function(elem, str){
+            return (elem.id.indexOf(str) != -1);
+        }
+    
+        // 指定タグ名かどうか
+        this.eq_tag = function(elem, str){
+            return (elem.tagName.toUpperCase() == str.toUpperCase());
+        }
+    
+        // href属性に指定文字列を含むかどうか
+        this.inc_href = function(elem, str){
+            var href = elem.getAttribute('href');
+            if(href !== null){
+                return (href.indexOf(str) != -1);
+            }else{
+                return false;
             }
         }
     }
-}
 
-function disableElementsWhatIncludesThisHref(refs, mode){
-    var tags = document.getElementsByTagName("*");
-    for(i=0;i < tags.length;i++){
-        var elem = tags[i];
-        var ref = elem.getAttribute('href');
-        var fnd = false;
-        if(ref != null){
-            for(var j = 0 ; j < refs.length ; j++){
-                fnd = ref.indexOf(refs[j]) != -1; //文字列が存在するかどうか
-                if(fnd){ //存在する場合
-                    disableThisElement(elem, mode, "disableElementsWhatIncludesThisHref");
-                    break;
+    // イベント登録 - DOMContentLoaded
+    if(typeof timingAndsettings.ready == 'object'){
+        // document.addEventListener("DOMContentLoaded", function() { // <- note:greasemonkeyでは不要
+            console.log("Disabling Start");
+            disableElements(timingAndsettings.ready);
+            console.log("Disabling End");
+        // });
+    }
+
+    // // イベント登録 - window.onload
+    if(typeof timingAndsettings.onload == 'object'){
+        window.addEventListener("load", function(){
+            console.log("Disabling Start");
+            disableElements(timingAndsettings.onload);
+            console.log("Disabling End");
+        }, false);
+    }
+
+    function disableElements(settings){
+        var elements = document.getElementsByTagName("*");
+    
+        //DOM要素網羅ループ
+        for(var idxOfElement = 0 ; idxOfElement < elements.length ; idxOfElement++){
+    
+            var element = elements[idxOfElement];
+    
+            //設定網羅ループ
+            for(var idxOfSetting = 0 ; idxOfSetting < settings.length ; idxOfSetting++){
+    
+                var setting = settings[idxOfSetting];
+                var func_judger;
+
+                if(typeof setting.judger == 'function'){ //関数の直接指定の場合
+                    func_judger = setting.judger;
+
+                }else{ //判定関数リストから指定の場合
+                    func_judger = judgers[setting.judger];
+                }
+                
+
+                if(Array.isArray(setting.specifier)){ //配列の場合
+                        
+                    //キーワード網羅ループ
+                    for(var idxOfSpecifier = 0 ; idxOfSpecifier < setting.specifier.length ; idxOfSpecifier++){
+                        var oneOfSpecifier = setting.specifier[idxOfSpecifier];
+                        if(func_judger(element, oneOfSpecifier)){ //判定結果trueの場合
+                            disable(element, setting.mode);
+                        }
+                    }
+                    
+                }else{ //配列でない場合
+                    if(func_judger(element, setting.specifier)){ //判定結果trueの場合
+                        disable(element, setting.mode);
+                    }
                 }
             }
         }
     }
-}
-
-function disableThisElement(elem, mode, whoCalled){
-    var succeeded = true;
-    switch(mode){
-        case "none":
-            elem.style.display = "none";
+    
+    function disable(elem, mode){
+        switch(mode){
+            case "none":
+            {
+                beforeDesabling(elem, mode);
+                elem.style.display = "none";
+                
+            }
             break;
 
-        case "hidden":
-            elem.style.visibility = "hidden";
+            case "hidden":
+            {
+                beforeDesabling(elem, mode);
+                elem.style.visibility = "hidden";
+            }
             break;
 
-        default:
-            succeeded = false;
+            case "remove":
+            {
+                beforeDesabling(elem, mode);
+                elem.remove();
+            }
             break;
+            
+    
+            default:
+            {
+                var errorMessage = "Unknown mode \`" + mode + "\` specified.";
+                throw new Error(errorMessage);
+            }
+            break;
+        }
     }
 
-    if(succeeded){
-        console.log(`following element is disabled by "${whoCalled}". tagName:"${elem.tagName}", id:"${elem.id}", className:"${elem.className}, href:"${elem.getAttribute('href')}"`);
-    }else{
-        console.warn(`unknown disabling type "${mode}" specified by "${whoCalled}". tagName:"${elem.tagName}", id:"${elem.id}", className:"${elem.className}, href:"${elem.getAttribute('href')}"`);
+    function beforeDesabling(elem, mode){
+        console.log("Disabled \`" + getDomPath(elem).join('/') + "\`. mode:\`" + mode + "\`");
+        console.log(elem);
     }
-}
+
+    // DOM要素のPathを配列型で返却する
+    function getDomPath(el) {
+        
+        var stack = [];
+        
+        while ( el.parentNode !== null ) {
+            
+            var sibCount = 0;
+            var sibIndex = 0;
+            
+            for ( var i = 0; i < el.parentNode.childNodes.length; i++ ) {
+                var sib = el.parentNode.childNodes[i];
+                if ( sib.nodeName == el.nodeName ) {
+                    if ( sib === el ) {
+                        sibIndex = sibCount;
+                    }
+                    sibCount++;
+                }
+            }
+
+            if ( el.hasAttribute('id') && el.id != '' ) {
+                stack.unshift(el.nodeName.toLowerCase() + '#' + el.id);
+            } else if ( sibCount > 1 ) {
+                stack.unshift(el.nodeName.toLowerCase() + ':eq(' + sibIndex + ')');
+            } else {
+                stack.unshift(el.nodeName.toLowerCase());
+            }
+
+            el = el.parentNode;
+        }
+        
+        return stack.slice(1); // removes the html element
+    }
+
+})();
